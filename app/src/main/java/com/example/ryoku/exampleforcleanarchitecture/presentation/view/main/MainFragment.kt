5c259f913c58732a35c4d72de271e7414a437c23
ryokuhei <r.sato@solt-inc.com>
2018-01-23 09:42:57 +0900

    add touch event

--- app/src/main/java/com/example/ryoku/exampleforcleanarchitecture/presentation/view/main/MainFragment.kt
+++ app/src/main/java/com/example/ryoku/exampleforcleanarchitecture/presentation/view/main/MainFragment.kt
@@ -1,7 +1,6 @@
 package com.example.ryoku.exampleforcleanarchitecture.presentation.view.main
 
 import android.content.Context
-import android.content.Intent
 import android.support.v4.app.Fragment
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
@@ -13,6 +12,7 @@ import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RelativeLayout
 import com.example.ryoku.exampleforcleanarchitecture.R
 import com.example.ryoku.exampleforcleanarchitecture.domain.model.User
 import com.example.ryoku.exampleforcleanarchitecture.presentation.presenter.MainPresenter
@@ -54,26 +54,32 @@ class MainFragment: Fragment(), MainFragmentDelegate, View.OnClickListener {
         ageET = view?.findViewById<EditText>(R.id.age_editText) as EditText
 
         view?.findViewById<Button>(R.id.register_button).setOnClickListener(this)
+        view?.findViewById<RelativeLayout>(R.id.layout_rl).setOnClickListener(this)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         activity.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
-
-//        val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//        imm.hideSoftInputFromWindow(view?.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
     }
 
     // createUser
     override fun onClick(view: View?) {
+        val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view?.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+
+
         when(view?.id) {
             R.id.register_button -> {
                 val firstName = firstNameET.text.toString()
                 val lastName = lastNameET.text.toString()
-                val age = Integer.parseInt(ageET.text.toString())
-                val user = User(firstName,lastName, age)
-                this.presenter.register(user)
+                val ageString =ageET.text.toString()
+                if (!firstName.isEmpty() && !lastName.isEmpty() && !ageString.isEmpty()) {
+                    val age = Integer.parseInt(ageString)
+                    val user = User(firstName, lastName, age)
+
+                    this.presenter.register(user)
+                }
             }
             R.id.layout_rl -> {
                 Log.d("layout", "clicked")
